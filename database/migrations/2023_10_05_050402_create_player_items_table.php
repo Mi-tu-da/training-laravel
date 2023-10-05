d3492415f0942a33bbc5fea415417e3793d6e6eb
Mi-tu-da <rinfo141009@icloud.com>
2023-10-05 14:18:04 +0900

    もう一度Up

--- database/migrations/2023_10_05_050402_create_player_items_table.php
+++ database/migrations/2023_10_05_050402_create_player_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePlayerItemsTable extends Migration
+class PlayerItem extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,20 @@ class CreatePlayerItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('_player__items', function (Blueprint $table) {
-            $table->id();
+        Schema::create('player_items', function (Blueprint $table) {
+
+            //符号なし整数を用意
+            $table->unsignedBigInteger('player_id')->comment("プレイヤーID");
+            $table->unsignedBigInteger('item_id')->comment("アイテムID");
+
+            //カウント用整数用意
+            $table->integer('count')->default(0)->comment("所持");
+
+            //タイムスタンプのような機能をテーブルに追加する
             $table->timestamps();
+            
+            //player_id と item_id の組み合わせが主キー
+            $table->primary(['player_id', 'item_id']);
         });
     }
 
@@ -26,6 +37,7 @@ class CreatePlayerItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('_player__items');
+        //up メソッドで行った変更を元に戻す処理
+        Schema::dropIfExists('player_items');
     }
 }
